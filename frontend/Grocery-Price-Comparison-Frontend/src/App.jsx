75423583fe595bbc4b2478d8dc0f38ebcc695287
Ernestas Pazdrazdis <ernestas.pazd@gmail.com>
2026-09-16 23:45:47 +0300

    Faktine klaida pataisyta.

--- frontend/Grocery-Price-Comparison-Frontend/src/App.jsx
+++ frontend/Grocery-Price-Comparison-Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <h1>Ernestas lopas</h1>
+          <h1>Benas lopas</h1>
         </div>
       </section>
     </>
