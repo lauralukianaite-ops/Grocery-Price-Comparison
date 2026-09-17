daa215d35c233661c7831a960bd2ceff0048d3d0
Augustas P <augustaspovil@gmail.com>
2026-09-17 21:19:32 +0300

    add postgres, create folders, basic entities and dbsets

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Price>  Prices { get; set; }
+    public DbSet<Store> Stores { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Price>().
+             HasOne(p => p.Store)
+            .WithMany(s => s.Prices)
+            .HasForeignKey(p => p.StoreId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Price>()
+            .HasOne(p => p.Item)
+            .WithMany(i => i.Prices)
+            .HasForeignKey(p => p.ItemId)
+            .OnDelete(DeleteBehavior.Cascade);
+            
+    }
+
+}
---
+++ backend/Entities/Price.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Entities;
+
+public class Price
+{
+    public int Id { get; set; }
+    public int StoreId { get; set; }
+    public int ItemId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime RecordedAt { get; set; } = DateTime.UtcNow;
+    public Store Store { get; set; }
+    public Item Item { get; set; }
+}
