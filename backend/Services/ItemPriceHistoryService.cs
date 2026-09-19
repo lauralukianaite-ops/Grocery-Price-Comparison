a9e19b847178ee931dcedddd55f318eefb01db68
Augustas P <augustaspovil@gmail.com>
2026-09-19 13:16:04 +0300

    feat: add dbcontext to service, implement getpricehistory query, endpoint should now work

--- backend/Services/ItemPriceHistoryService.cs
+++ backend/Services/ItemPriceHistoryService.cs
@@ -1,12 +1,31 @@
-﻿using backend.DTOs;
+﻿using backend.Data;
+using backend.DTOs;
 using backend.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Services;
 
 public class ItemPriceHistoryService : IItemPriceHistoryService
 {
+    private readonly AppDbContext _dbContext;
+    public ItemPriceHistoryService(AppDbContext db)
+    {
+        _dbContext = db;
+    }
     public async Task<List<PricePointDto>?> GetPriceHistoryAsync(int itemId)
     {
-        throw new NotImplementedException();
+        var itemsExist = await _dbContext.Items.AnyAsync(i => i.Id == itemId);
+        if (!itemsExist) return null;
+        
+        return await _dbContext.Prices
+            .Where(p => p.ItemId == itemId)
+            .OrderBy(p => p.RecordedAt)
+            .Select(p => new PricePointDto(
+                p.StoreId,
+                p.Store.Name,
+                p.Amount,
+                p.RecordedAt
+                ))
+            .ToListAsync();
     }
 }
