4484708e2d2c84eb9ea93b53179fa8f03da65301
Augustas P <augustaspovil@gmail.com>
2026-09-26 22:08:05 +0300

    feature: generating migration for cost rename and retail cost column

---
+++ backend/Migrations/20260926185409_AddRetailCostToPrice.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRetailCostToPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Prices",
+                newName: "Cost");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RetailCost",
+                table: "Prices",
+                type: "numeric(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RetailCost",
+                table: "Prices");
+
+            migrationBuilder.RenameColumn(
+                name: "Cost",
+                table: "Prices",
+                newName: "Amount");
+        }
+    }
+}
